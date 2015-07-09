7089ac0c4a2c01a00d17b98f22b64fb9c2b3ebdd
yoniji <hello.hyu@gmail.com>
2015-07-09 15:22:26 +0800

    first commit

---
+++ app/modules/common/Controller.js
@@ -0,0 +1,11 @@
+﻿define(['backbone', 'marionette','modules/welcome/WelcomeView'],
+    function (Backbone, Marionette, WelcomeView) {
+        return Marionette.Controller.extend({
+            initialize: function (options) {
+
+            },
+            welcome: function() {
+                var welcome = new WelcomeView();
+            }
+        });
+    });
---
+++ app/modules/welcome/WelcomeView.js
@@ -0,0 +1,27 @@
+﻿define(['backbone', 'marionette', 'mustache', 'jquery', 'text!modules/welcome/welcome.html', 'iscroll'],
+    function(Backbone, Marionette, Mustache, $, template) {
+
+        return Marionette.ItemView.extend({
+            template: function(serialized_model) {
+                return Mustache.render(template, serialized_model);
+            },
+            ui: {
+
+            },
+            events: {
+
+            },
+            initialize: function() {
+                app.rootView.updatePrimaryRegion(this);
+
+            },
+            onShow: function() {
+                this.iscroll = new IScroll(this.$el[0]);
+
+            },
+            onDestroy: function() {
+                this.stopListening();
+            },
+            id: 'welcomeWrapper'
+        });
+    });
